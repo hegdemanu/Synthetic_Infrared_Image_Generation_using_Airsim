6bcdc67cdc6bd109b9ce8aadb424a09e13a3592f
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2024-12-15 00:04:43 +0530

    Create init__.py

---
+++ losses/init__.py
@@ -0,0 +1,9 @@
+from .sir_losses import SIRLoss, AdversarialLoss, CycleConsistencyLoss, InfraredLoss, StructureLoss
+
+__all__ = [
+    'SIRLoss',
+    'AdversarialLoss', 
+    'CycleConsistencyLoss',
+    'InfraredLoss',
+    'StructureLoss'
+]
