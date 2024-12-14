a0a20ab84e1a1026c60cc5c0ba5c8a9c4fa0e2a3
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2024-12-14 23:53:00 +0530

    Setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="sir-gan",
     version="0.1.0",
     description="Synthetic IR Image Refinement using Adversarial Learning",
-    author="Your Name",
+    author="Manu_hegde",
     author_email="your.email@example.com",
     packages=find_packages(),
     install_requires=[
